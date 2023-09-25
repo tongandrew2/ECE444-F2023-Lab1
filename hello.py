610ac2bce8e6fe88d4eac5279b8ba30f29d258f8
tongandrew2 <64707450+tongandrew2@users.noreply.github.com>
2023-09-24 21:08:34 -0400

    Completion of Activity 3.

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/user/<name>')
+@app.route('/')
+def index():
+ return render_template('index.html', current_time=datetime.utcnow())
+@app.route('/user/<name>/')
 def user(name):
- return '<h1>Hello, {}!</h1>'.format(name)
+ return render_template('user.html', name=name, current_time=datetime.utcnow())
